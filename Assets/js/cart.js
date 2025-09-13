bda659e057b51a53036367096f243196dd0b276a
devn-ai <v.kozak.digitai@gmail.com>
2025-09-13 23:11:41 +0200

    Update cart.js

--- Assets/js/cart.js
+++ Assets/js/cart.js
@@ -17,17 +17,6 @@ fetch('/shoes_store_tg/Assets/db.json')
       `;  
       catalog.appendChild(card);
     });
-  });
-window.addEventListener('DOMContentLoaded', function () {
-  // Перевіряємо, чи підключений Telegram WebApp
-  if (window.Telegram && Telegram.WebApp && Telegram.WebApp.initDataUnsafe && Telegram.WebApp.initDataUnsafe.user) {
-    const userId = Telegram.WebApp.initDataUnsafe.user.id;
-    document.getElementById('customer-id').value = userId;
-  } else {
-    // fallback, якщо доступу до Telegram WebApp немає (наприклад, не з Telegram)
-    document.getElementById('customer-id').value = '';
-  }
-});
 
 
 let cart = [];
