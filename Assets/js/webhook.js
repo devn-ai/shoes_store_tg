bfcc509ac4429ad3f7e94f66a0f0d5101aa41204
devn-ai <v.kozak.digitai@gmail.com>
2025-09-13 23:44:58 +0200

    Update webhook.js

--- Assets/js/webhook.js
+++ Assets/js/webhook.js
@@ -4,8 +4,9 @@ window.addEventListener('DOMContentLoaded', function () {
   if (window.Telegram && Telegram.WebApp && Telegram.WebApp.initDataUnsafe && Telegram.WebApp.initDataUnsafe.user) {
     const userId = Telegram.WebApp.initDataUnsafe.user.id;
     document.getElementById('customer-id').value = userId;
-  } else {
+  } //else {
     // fallback, якщо доступу до Telegram WebApp немає (наприклад, не з Telegram)
-    document.getElementById('customer-id').value = '';
-  }
+    //document.getElementById('customer-id').value = '';
+  //}
 });
+
